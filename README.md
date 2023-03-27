8aa498b552d0c58a872e652cf7dcbfccddc409b8
Sergimayol <sergi2808@gmail.com>
2023-03-27 20:17:07 +0200

    Inicio opengl

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# opengl-projects
+# Proyectos OpenGL
+
+## Desarollar
+
+Para compilar los programas se emplea `nobuild.c`. Para ello, primero se debe de compilar este programa y después ejecutarlo.
+
+- Ejemplo:
+
+```bash
+# Si no se tiene compilado nobuild compilarlo
+gcc -o nobuild nobuild.c
+# Para ejecutar y compilar programas
+./nobuild
+```
--- nobuild.c
+++ nobuild.c
@@ -1,24 +1,27 @@
 #define NOBUILD_IMPLEMENTATION
 #include "./nobuild.h"
 
-#define CFLAGS "-Wall", "-Wextra", "-Werror", "-std=c99"
-#define COMPILER "cc"
+#define CFLAGS "-Wall", "-Wextra", "-Werror", "-std=c99", "-lglut", "-lGLU", "-lGL"
+#define COMPILER "gcc"
 #define BUILD_DIR "build"
 #define BUILD_PATH(path) BUILD_DIR "/" path
 
-void compile_programs() 
+void compile_programs()
 {
-    CMD(COMPILER, CFLAGS, "-o", BUILD_PATH("hello"), "hello.c");
+    CMD(COMPILER, "-o", BUILD_PATH("etapa1"), "./src/etapa1.c", CFLAGS);
+    CMD(COMPILER, "-o", BUILD_PATH("etapa2"), "./src/etapa2.c", CFLAGS);
 }
 
 void clean_output_exes()
 {
-    CMD("rm", "-f", BUILD_PATH("hello"));
+    CMD("rm", "-f", BUILD_PATH("etapa1"));
+    CMD("rm", "-f", BUILD_PATH("etapa2"));
 }
 
 void run_programs()
 {
-    CMD(BUILD_PATH("hello"));
+    CMD(BUILD_PATH("etapa1"));
+    CMD(BUILD_PATH("etapa2"));
 }
 
 int main(int argc, char **argv)
@@ -26,7 +29,7 @@ int main(int argc, char **argv)
     GO_REBUILD_URSELF(argc, argv);
     MKDIRS(BUILD_DIR);
     compile_programs();
-    run_programs();
-    clean_output_exes();
+    //    run_programs();
+    //    clean_output_exes();
     return 0;
 }
