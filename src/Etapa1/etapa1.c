779e863b41304dc964641565246cf91d717e2ac7
Rubén Palmer <40397192+Zygmut@users.noreply.github.com>
2023-04-19 19:00:41 +0200

    Update stages to use template and such

--- src/Etapa1/etapa1.c
+++ src/Etapa1/etapa1.c
@@ -1,122 +1,119 @@
-#include <GL/glut.h>
-#include <GL/gl.h>
-#include <GL/glu.h>
 #include "../templates/base.h"
 
-const int W_WIDTH = 500; // Tama�o incial de la ventana
+const int W_WIDTH = 500; // Tamaño incial de la ventana
 const int W_HEIGHT = 500;
 const float SQR_SIZE = 0.2f;
-GLfloat fAngulo; // Variable que indica el �ngulo de rotaci�n de los ejes.
+GLfloat fAngulo; // Variable que indica el ángulo de rotación de los ejes.
 
 // Función que controla la proporción al cambiar el tamaño
 void reshape(int width, int height)
 {
-    const float ar_origin = (float)W_WIDTH / (float)W_HEIGHT;
-    const float ar_new = (float)width / (float)height;
+	const float ar_origin = (float)W_WIDTH / (float)W_HEIGHT;
+	const float ar_new = (float)width / (float)height;
 
-    float scale_w = (float)width / (float)W_WIDTH;
-    float scale_h = (float)height / (float)W_HEIGHT;
-    if (ar_new > ar_origin)
-    {
-        scale_w = scale_h;
-    }
-    else
-    {
-        scale_h = scale_w;
-    }
+	float scale_w = (float)width / (float)W_WIDTH;
+	float scale_h = (float)height / (float)W_HEIGHT;
+	if (ar_new > ar_origin)
+	{
+		scale_w = scale_h;
+	}
+	else
+	{
+		scale_h = scale_w;
+	}
 
-    float margin_x = (width - W_WIDTH * scale_w) / 2;
-    float margin_y = (height - W_HEIGHT * scale_h) / 2;
+	float margin_x = (width - W_WIDTH * scale_w) / 2;
+	float margin_y = (height - W_HEIGHT * scale_h) / 2;
 
-    glViewport(margin_x, margin_y, W_WIDTH * scale_w, W_HEIGHT * scale_h);
+	glViewport(margin_x, margin_y, W_WIDTH * scale_w, W_HEIGHT * scale_h);
 }
 
-// Funci�n que visualiza la escena OpenGL
+// Función que visualiza la escena OpenGL
 void display(void)
 {
-    // Borramos la escena
-    glClear(GL_COLOR_BUFFER_BIT);
+	// Borramos la escena
+	glClear(GL_COLOR_BUFFER_BIT);
 
-    glPushMatrix();
-    // Rotamos las proximas primitivas
-    glRotatef(fAngulo, 0.0f, 0.0f, 1.0f);
-    // Creamos a continuaci�n dibujamos los tres poligonos
-    glBegin(GL_POLYGON);
-    glColor4f(LIGHT);
-    glVertex3f(0.0f, 0.0f, 0.0f);
-    glColor4f(RED);
-    glVertex3f(1.0f, 0.0f, 0.0f);
-    glColor4f(GREEN);
-    glVertex3f(-0.5f, 0.866f, 0.0f);
-    glEnd();
+	glPushMatrix();
+	// Rotamos las proximas primitivas
+	glRotatef(fAngulo, 0.0f, 0.0f, 1.0f);
+	// Creamos a continuación dibujamos los tres poligonos
+	glBegin(GL_POLYGON);
+	glColor4f(LIGHT);
+	glVertex3f(0.0f, 0.0f, 0.0f);
+	glColor4f(RED);
+	glVertex3f(1.0f, 0.0f, 0.0f);
+	glColor4f(GREEN);
+	glVertex3f(-0.5f, 0.866f, 0.0f);
+	glEnd();
 
-    glBegin(GL_POLYGON);
-    glColor4f(LIGHT);
-    glVertex3f(0.0f, 0.0f, 0.0f);
-    glColor4f(RED);
-    glVertex3f(1.0f, 0.0f, 0.0f);
-    glColor4f(BLUE);
-    glVertex3f(-0.5f, -0.866f, 0.0f);
-    glEnd();
+	glBegin(GL_POLYGON);
+	glColor4f(LIGHT);
+	glVertex3f(0.0f, 0.0f, 0.0f);
+	glColor4f(RED);
+	glVertex3f(1.0f, 0.0f, 0.0f);
+	glColor4f(BLUE);
+	glVertex3f(-0.5f, -0.866f, 0.0f);
+	glEnd();
 
-    glBegin(GL_POLYGON);
-    glColor4f(LIGHT);
-    glVertex3f(0.0f, 0.0f, 0.0f);
-    glColor4f(GREEN);
-    glVertex3f(-0.5f, 0.866f, 0.0f);
-    glColor4f(BLUE);
-    glVertex3f(-0.5f, -0.866f, 0.0f);
-    glEnd();
+	glBegin(GL_POLYGON);
+	glColor4f(LIGHT);
+	glVertex3f(0.0f, 0.0f, 0.0f);
+	glColor4f(GREEN);
+	glVertex3f(-0.5f, 0.866f, 0.0f);
+	glColor4f(BLUE);
+	glVertex3f(-0.5f, -0.866f, 0.0f);
+	glEnd();
 
-    // Cuadrado
-    glBegin(GL_POLYGON);
-    glColor4f(DARK);
-    glVertex2f(-SQR_SIZE, SQR_SIZE);
-    glVertex2f(SQR_SIZE, SQR_SIZE);
-    glVertex2f(SQR_SIZE, -SQR_SIZE);
-    glVertex2f(-SQR_SIZE, -SQR_SIZE);
-    glEnd();
+	// Cuadrado
+	glBegin(GL_POLYGON);
+	glColor4f(DARK);
+	glVertex2f(-SQR_SIZE, SQR_SIZE);
+	glVertex2f(SQR_SIZE, SQR_SIZE);
+	glVertex2f(SQR_SIZE, -SQR_SIZE);
+	glVertex2f(-SQR_SIZE, -SQR_SIZE);
+	glEnd();
 
-    glPopMatrix();
+	glPopMatrix();
+	glutSwapBuffers();
 
-    glFlush();
+	glFlush();
 }
 
-// Funci�n que se ejecuta cuando el sistema no esta ocupado
+// Función que se ejecuta cuando el sistema no esta ocupado
 void idle(void)
 {
-    // Incrementamos el �ngulo
-    fAngulo += 0.3f;
-    // Si es mayor que dos pi la decrementamos
-    if (fAngulo > 360)
-        fAngulo -= 360;
-    // Indicamos que es necesario repintar la pantalla
-    glutPostRedisplay();
+	// Incrementamos el ángulo
+	fAngulo += 0.3f;
+	// Si es mayor que dos pi la decrementamos
+	fAngulo = fmod(fAngulo, 360);
+	// Indicamos que es necesario repintar la pantalla
+	glutPostRedisplay();
 }
 
-// Funci�n principal
+// Función principal
 int main(int argc, char **argv)
 {
-    // Inicializamos la librer�a GLUT
-    glutInit(&argc, argv);
+	// Inicializamos la librería GLUT
+	glutInit(&argc, argv);
 
-    // Indicamos como ha de ser la nueva ventana
-    glutInitWindowPosition(100, 100);
-    glutInitWindowSize(W_WIDTH, W_HEIGHT);
-    glutInitDisplayMode(GLUT_RGBA | GLUT_SINGLE);
+	// Indicamos como ha de ser la nueva ventana
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(W_WIDTH, W_HEIGHT);
+	glutInitDisplayMode(GLUT_RGBA | GLUT_SINGLE);
 
-    // Creamos la nueva ventana
-    glutCreateWindow("Mi primera Ventana");
+	// Creamos la nueva ventana
+	glutCreateWindow("Etapa 1");
 
-    // Indicamos cuales son las funciones de redibujado e idle
-    glutDisplayFunc(display);
-    glutIdleFunc(idle);
-    glutReshapeFunc(reshape);
+	// Indicamos cuales son las funciones de redibujado e idle
+	glutDisplayFunc(display);
+	glutIdleFunc(idle);
+	glutReshapeFunc(reshape);
 
-    // El color de fondo ser� el negro (RGBA, RGB + Alpha channel)
-    glClearColor(BG_COLOR);
+	// El color de fondo será el negro (RGBA, RGB + Alpha channel)
+	glClearColor(BG_COLOR);
 
-    // Comienza la ejecuci�n del core de GLUT
-    glutMainLoop();
-    return 0;
+	// Comienza la ejecución del core de GLUT
+	glutMainLoop();
+	return 0;
 }
--- src/Etapa2/etapa2.c
+++ src/Etapa2/etapa2.c
@@ -1,152 +1,149 @@
-#include <GL/glut.h>
-#include <GL/gl.h>
-#include <GL/glu.h>
 #include "../templates/base.h"
 
-const int W_WIDTH = 600; // Tama�o incial de la ventana
+const int W_WIDTH = 600; // Tamaño incial de la ventana
 const int W_HEIGHT = 600;
-GLfloat fAngulo; // Variable que indica el �ngulo de rotaci�n de los ejes.
+GLfloat fAngulo; // Variable que indica el ángulo de rotación de los ejes.
 
-// Funci�n que visualiza la escena OpenGL
+// Función que visualiza la escena OpenGL
 void display(void)
 {
-    // Borramos la escena
-    glClear(GL_COLOR_BUFFER_BIT);
+	// Borramos la escena
+	glClear(GL_COLOR_BUFFER_BIT);
 
-    glMatrixMode(GL_MODELVIEW); // set the modelview matrix
-    glLoadIdentity();           // load the identity matrix
+	glMatrixMode(GL_MODELVIEW); // set the modelview matrix
+	glLoadIdentity();			// load the identity matrix
 
-    glEnable(GL_LINE_SMOOTH); // enable anti-aliasing
+	glEnable(GL_LINE_SMOOTH); // enable anti-aliasing
 
-    const float TAM_ESC = 1.0;
+	const float TAM_ESC = 1.0;
 
-    // ---- CUADRANTE ARRIBA - IZQ -------
-    glTranslatef(0.2f, 0.2f, 0.2f); // Translación
-    glBegin(GL_QUADS);
-    glColor4f(LIGHT);                              // blanco
-    glVertex2f(-0.75f * TAM_ESC, 0.25f * TAM_ESC); // arriba-izquierda
-    glVertex2f(-0.75f * TAM_ESC, 0.75f * TAM_ESC); // abajo-izquierda
-    glVertex2f(-0.25f * TAM_ESC, 0.75f * TAM_ESC); // abajo-derecha
-    glVertex2f(-0.25f * TAM_ESC, 0.25f * TAM_ESC); // arriba-derecha
-    glEnd();
-    glLoadIdentity();
+	// ---- CUADRANTE ARRIBA - IZQ -------
+	glTranslatef(0.2f, 0.2f, 0.2f); // Translación
+	glBegin(GL_QUADS);
+	glColor4f(LIGHT);							   // blanco
+	glVertex2f(-0.75f * TAM_ESC, 0.25f * TAM_ESC); // arriba-izquierda
+	glVertex2f(-0.75f * TAM_ESC, 0.75f * TAM_ESC); // abajo-izquierda
+	glVertex2f(-0.25f * TAM_ESC, 0.75f * TAM_ESC); // abajo-derecha
+	glVertex2f(-0.25f * TAM_ESC, 0.25f * TAM_ESC); // arriba-derecha
+	glEnd();
+	glLoadIdentity();
 
-    // ---- CUADRANTE ABAJO - IZQ -------
-    glRotatef(45.0f, -0.5f, -0.5f, 0.0f); // Rotación
-    glBegin(GL_QUADS);
-    glColor4f(LIGHT);                               // blanco
-    glVertex2f(-0.75f * TAM_ESC, -0.25f * TAM_ESC); // arriba-izquierda
-    glVertex2f(-0.75f * TAM_ESC, -0.75f * TAM_ESC); // abajo-izquierda
-    glVertex2f(-0.25f * TAM_ESC, -0.75f * TAM_ESC); // abajo-derecha
-    glVertex2f(-0.25f * TAM_ESC, -0.25f * TAM_ESC); // arriba-derecha
-    glEnd();
-    glLoadIdentity();
+	// ---- CUADRANTE ABAJO - IZQ -------
+	glRotatef(45.0f, -0.5f, -0.5f, 0.0f); // Rotación
+	glBegin(GL_QUADS);
+	glColor4f(LIGHT);								// blanco
+	glVertex2f(-0.75f * TAM_ESC, -0.25f * TAM_ESC); // arriba-izquierda
+	glVertex2f(-0.75f * TAM_ESC, -0.75f * TAM_ESC); // abajo-izquierda
+	glVertex2f(-0.25f * TAM_ESC, -0.75f * TAM_ESC); // abajo-derecha
+	glVertex2f(-0.25f * TAM_ESC, -0.25f * TAM_ESC); // arriba-derecha
+	glEnd();
+	glLoadIdentity();
 
-    // ---- CUADRANTE ABAJO - DER-------
-    glScalef(1.5f, 1.5f, 0.0f); // Escalado
-    glBegin(GL_QUADS);
-    glColor4f(LIGHT);                              // blanco
-    glVertex2f(0.75f * TAM_ESC, -0.25f * TAM_ESC); // arriba-izquierda
-    glVertex2f(0.75f * TAM_ESC, -0.75f * TAM_ESC); // abajo-izquierda
-    glVertex2f(0.25f * TAM_ESC, -0.75f * TAM_ESC); // abajo-derecha
-    glVertex2f(0.25f * TAM_ESC, -0.25f * TAM_ESC); // arriba-derecha
-    glEnd();
-    glLoadIdentity();
+	// ---- CUADRANTE ABAJO - DER-------
+	glScalef(1.5f, 1.5f, 0.0f); // Escalado
+	glBegin(GL_QUADS);
+	glColor4f(LIGHT);							   // blanco
+	glVertex2f(0.75f * TAM_ESC, -0.25f * TAM_ESC); // arriba-izquierda
+	glVertex2f(0.75f * TAM_ESC, -0.75f * TAM_ESC); // abajo-izquierda
+	glVertex2f(0.25f * TAM_ESC, -0.75f * TAM_ESC); // abajo-derecha
+	glVertex2f(0.25f * TAM_ESC, -0.25f * TAM_ESC); // arriba-derecha
+	glEnd();
+	glLoadIdentity();
 
-    // ---- CUADRANTE ARRIBA - DER-------
-    GLfloat m[4 * 4] = {
-        1.0f, -0.25f, 0.3f, 0.0f,
-        0.7f, 1.0f, 0.0f, 0.0f,
-        0.0f, 0.3f, 1.0f, 0.0f,
-        0.0f, 0.0f, 0.0f, 1.0f};
-    glMultMatrixf(m);
-    glBegin(GL_QUADS);
-    glColor4f(LIGHT);                             // blanco
-    glVertex2f(0.75f * TAM_ESC, 0.25f * TAM_ESC); // arriba-izquierda
-    glVertex2f(0.75f * TAM_ESC, 0.75f * TAM_ESC); // abajo-izquierda
-    glVertex2f(0.25f * TAM_ESC, 0.75f * TAM_ESC); // abajo-derecha
-    glVertex2f(0.25f * TAM_ESC, 0.25f * TAM_ESC); // arriba-derecha
-    glEnd();
-    glLoadIdentity();
+	// ---- CUADRANTE ARRIBA - DER-------
+	GLfloat m[4 * 4] = {
+		1.0f, -0.25f, 0.3f, 0.0f,
+		0.7f, 1.0f, 0.0f, 0.0f,
+		0.0f, 0.3f, 1.0f, 0.0f,
+		0.0f, 0.0f, 0.0f, 1.0f};
+	glMultMatrixf(m);
+	glBegin(GL_QUADS);
+	glColor4f(LIGHT);							  // blanco
+	glVertex2f(0.75f * TAM_ESC, 0.25f * TAM_ESC); // arriba-izquierda
+	glVertex2f(0.75f * TAM_ESC, 0.75f * TAM_ESC); // abajo-izquierda
+	glVertex2f(0.25f * TAM_ESC, 0.75f * TAM_ESC); // abajo-derecha
+	glVertex2f(0.25f * TAM_ESC, 0.25f * TAM_ESC); // arriba-derecha
+	glEnd();
+	glLoadIdentity();
 
-    // Ejes
-    glColor4f(PURE_WHITE);
-    // Eje x
-    glBegin(GL_LINES);
-    glVertex2f(1.0f, 0.0f);
-    glVertex2f(-1.0f, 0.0f);
-    glEnd();
+	// Ejes
+	glColor4f(PURE_WHITE);
+	// Eje x
+	glBegin(GL_LINES);
+	glVertex2f(1.0f, 0.0f);
+	glVertex2f(-1.0f, 0.0f);
+	glEnd();
 
-    // Eje y
-    glBegin(GL_LINES);
-    glVertex2f(0.0f, 1.0f);
-    glVertex2f(0.0f, -1.0f);
-    glEnd();
+	// Eje y
+	glBegin(GL_LINES);
+	glVertex2f(0.0f, 1.0f);
+	glVertex2f(0.0f, -1.0f);
+	glEnd();
 
-    glutSwapBuffers();
+	glutSwapBuffers();
 
-    glFlush();
+	glFlush();
 }
 
-// Funci�n que se ejecuta cuando el sistema no esta ocupado
+// Función que se ejecuta cuando el sistema no esta ocupado
 void idle(void)
 {
-    // Incrementamos el �ngulo
-    fAngulo += 0.3f;
-    // Si es mayor que dos pi la decrementamos
-    if (fAngulo > 360)
-        fAngulo -= 360;
-    // Indicamos que es necesario repintar la pantalla
-    glutPostRedisplay();
+	// Incrementamos el ángulo
+	fAngulo += 0.3f;
+	// Si es mayor que dos pi la decrementamos
+	if (fAngulo > 360)
+		fAngulo -= 360;
+	// Indicamos que es necesario repintar la pantalla
+	glutPostRedisplay();
 }
 
 void reshape(GLsizei width, GLsizei height)
 {
-    // Calcular el aspect ratio de la nueva ventana
-    if (height == 0)
-    {
-        height = 1; // Para evitar dividir por cero
-    }
-    GLfloat aspect = (GLfloat)width / (GLfloat)height;
-    // Hacer que el viewport cubra la nueva ventana
-    glViewport(0, 0, width, height);
-    // Hacer que el aspect ratio del área de dibujado sea igual al del viewport
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    if (width >= height)
-    {
-        glOrtho(-1.0 * aspect, 1.0 * aspect, -1.0, 1.0, -1.0, 1.0);
-    }
-    else
-    {
-        glOrtho(-1.0, 1.0, -1.0 / aspect, 1.0 / aspect, -1.0, 1.0);
-    }
-    glMatrixMode(GL_MODELVIEW);
+	// Calcular el aspect ratio de la nueva ventana
+	if (height == 0)
+	{
+		height = 1; // Para evitar dividir por cero
+	}
+	GLfloat aspect = (GLfloat)width / (GLfloat)height;
+	// Hacer que el viewport cubra la nueva ventana
+	glViewport(0, 0, width, height);
+	// Hacer que el aspect ratio del área de dibujado sea igual al del viewport
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	if (width >= height)
+	{
+		glOrtho(-1.0 * aspect, 1.0 * aspect, -1.0, 1.0, -1.0, 1.0);
+	}
+	else
+	{
+		glOrtho(-1.0, 1.0, -1.0 / aspect, 1.0 / aspect, -1.0, 1.0);
+	}
+	glMatrixMode(GL_MODELVIEW);
 }
 
-// Funci�n principal
+// Función principal
 int main(int argc, char **argv)
 {
-    // Inicializamos la librer�a GLUT
-    glutInit(&argc, argv);
+	// Inicializamos la librería GLUT
+	glutInit(&argc, argv);
 
-    // Indicamos como ha de ser la nueva ventana
-    glutInitWindowPosition(100, 100);
-    glutInitWindowSize(W_WIDTH, W_HEIGHT);
-    glutInitDisplayMode(GLUT_RGBA | GLUT_SINGLE);
+	// Indicamos como ha de ser la nueva ventana
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(W_WIDTH, W_HEIGHT);
+	glutInitDisplayMode(GLUT_RGBA | GLUT_SINGLE);
 
-    // Creamos la nueva ventana
-    glutCreateWindow("Etapa 2");
+	// Creamos la nueva ventana
+	glutCreateWindow("Etapa 2.1");
 
-    // Indicamos cuales son las funciones de redibujado e idle
-    glutDisplayFunc(display);
-    glutReshapeFunc(reshape);
-    glutIdleFunc(idle);
+	// Indicamos cuales son las funciones de redibujado e idle
+	glutDisplayFunc(display);
+	glutReshapeFunc(reshape);
+	glutIdleFunc(idle);
 
-    // El color de fondo ser� el negro (RGBA, RGB + Alpha channel)
-    glClearColor(BG_COLOR);
+	// El color de fondo será el negro (RGBA, RGB + Alpha channel)
+	glClearColor(BG_COLOR);
 
-    // Comienza la ejecuci�n del core de GLUT
-    glutMainLoop();
-    return 0;
+	// Comienza la ejecución del core de GLUT
+	glutMainLoop();
+	return 0;
 }
