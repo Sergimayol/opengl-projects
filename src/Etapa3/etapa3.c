2fc5e788106b5017e93b3b9c675de4548b2a040f
Sergimayol <sergi2808@gmail.com>
2023-05-03 21:02:39 +0200

    Nuevos colores + etapa 3 corregida y finalizada

--- src/Etapa3/etapa3.c
+++ src/Etapa3/etapa3.c
@@ -3,9 +3,9 @@
 
 const int W_WIDTH = 600; // Tamaño incial de la ventana
 const int W_HEIGHT = 600;
-const float ESCALADO_FIG = 0.5f;
+const float ESCALADO_FIG = 2.5f;
 GLfloat fAngulo;
-unsigned char fig = 'c';
+unsigned char fig = '1';
 bool displayAxis = true;
 bool displayWithWire = true;
 bool displayPlane = false;
@@ -13,10 +13,7 @@ bool displayPlane = false;
 void drawTeaPot()
 {
     glPushMatrix();
-    glLoadIdentity();
-    glRotatef(45.0f, 1.0f, 0.0f, 0.0f);   // Rotate the object around the x-axis
-    glRotatef(30.0f, 0.0f, 1.0f, 0.0f);   // Rotate the object around the y-axis
-    glRotatef(fAngulo, 0.0f, 0.0f, 1.0f); // rotate the cube
+    glRotatef(fAngulo, 0.0f, 1.0f, 0.0f); // rotate the cube
 
     // enable depth testing
     glEnable(GL_DEPTH_TEST);
@@ -24,7 +21,7 @@ void drawTeaPot()
     // set the color of the cube
     glColor3f(1.0f, 0.0f, 0.0f); // red
 
-    const GLdouble size = 1 * ESCALADO_FIG;
+    const GLdouble size = 0.5 * ESCALADO_FIG;
 
     if (displayWithWire)
     {
@@ -42,10 +39,7 @@ void drawTeaPot()
 void drawTetreaHedron()
 {
     glPushMatrix();
-    glLoadIdentity();
-    glRotatef(45.0f, 1.0f, 0.0f, 0.0f);   // Rotate the object around the x-axis
-    glRotatef(30.0f, 0.0f, 1.0f, 0.0f);   // Rotate the object around the y-axis
-    glRotatef(fAngulo, 0.0f, 0.0f, 1.0f); // rotate the cube
+    glRotatef(fAngulo, 0.0f, 1.0f, 0.0f); // rotate the cube
 
     // enable depth testing
     glEnable(GL_DEPTH_TEST);
@@ -69,9 +63,8 @@ void drawTetreaHedron()
 void drawDonut()
 {
     glPushMatrix();
-    glLoadIdentity();
     glRotatef(45.0f, 1.0f, 0.0f, 0.0f);   // Rotate the object around the x-axis
-    glRotatef(30.0f, 0.0f, 1.0f, 0.0f);   // Rotate the object around the y-axis
+    glRotatef(45.0f, 0.0f, 1.0f, 0.0f);   // Rotate the object around the y-axis
     glRotatef(fAngulo, 0.0f, 0.0f, 1.0f); // rotate the cube
 
     // enable depth testing
@@ -81,13 +74,14 @@ void drawDonut()
     glColor3f(1.0f, 0.0f, 0.0f); // red
 
     const int NUM_OF_SLICES = 20;
+    const double SIZE = 0.5;
     if (displayWithWire)
     {
-        glutWireTorus(1.0 * ESCALADO_FIG, 1.0 * ESCALADO_FIG, NUM_OF_SLICES, NUM_OF_SLICES); // Render a cone with base radius 1.0 and height 2.0
+        glutWireTorus(SIZE * ESCALADO_FIG, SIZE * ESCALADO_FIG, NUM_OF_SLICES, NUM_OF_SLICES); // Render a cone with base radius 1.0 and height 2.0
     }
     else
     {
-        glutSolidTorus(1.0 * ESCALADO_FIG, 1.0 * ESCALADO_FIG, NUM_OF_SLICES, NUM_OF_SLICES); // Render a cone with base radius 1.0 and height 2.0
+        glutSolidTorus(SIZE * ESCALADO_FIG, SIZE * ESCALADO_FIG, NUM_OF_SLICES, NUM_OF_SLICES); // Render a cone with base radius 1.0 and height 2.0
     }
 
     glEnd();
@@ -97,10 +91,7 @@ void drawDonut()
 void drawTriangle()
 {
     glPushMatrix();
-    glLoadIdentity();
-    glRotatef(45.0f, 1.0f, 0.0f, 0.0f);   // Rotate the object around the x-axis
-    glRotatef(30.0f, 0.0f, 1.0f, 0.0f);   // Rotate the object around the y-axis
-    glRotatef(fAngulo, 0.0f, 0.0f, 1.0f); // rotate the cube
+    glRotatef(fAngulo, 0.0f, 1.0f, 0.0f); // rotate the cube
 
     // enable depth testing
     glEnable(GL_DEPTH_TEST);
@@ -109,13 +100,14 @@ void drawTriangle()
     glColor3f(1.0f, 0.0f, 0.0f); // red
 
     const int NUM_OF_SLICES = 20;
+    const double SIZE = 0.5;
     if (displayWithWire)
     {
-        glutWireCone(1.0 * ESCALADO_FIG, 1.0 * ESCALADO_FIG, NUM_OF_SLICES, NUM_OF_SLICES); // Render a cone with base radius 1.0 and height 2.0
+        glutWireCone(SIZE * ESCALADO_FIG, SIZE * ESCALADO_FIG, NUM_OF_SLICES, NUM_OF_SLICES); // Render a cone with base radius 1.0 and height 2.0
     }
     else
     {
-        glutSolidCone(1.0 * ESCALADO_FIG, 1.0 * ESCALADO_FIG, NUM_OF_SLICES, NUM_OF_SLICES); // Render a cone with base radius 1.0 and height 2.0
+        glutSolidCone(SIZE * ESCALADO_FIG, SIZE * ESCALADO_FIG, NUM_OF_SLICES, NUM_OF_SLICES); // Render a cone with base radius 1.0 and height 2.0
     }
 
     glEnd();
@@ -125,10 +117,7 @@ void drawTriangle()
 void drawCube()
 {
     glPushMatrix();
-    glLoadIdentity();
-    glRotatef(45.0f, 1.0f, 0.0f, 0.0f);   // Rotate the object around the x-axis
-    glRotatef(-30.0f, 0.0f, 1.0f, 0.0f);  // Rotate the object around the y-axis
-    glRotatef(fAngulo, 0.0f, 0.0f, 1.0f); // rotate the cube
+    glRotatef(fAngulo, 0.0f, 1.0f, 0.0f); // rotate the cube
 
     // enable depth testing
     glEnable(GL_DEPTH_TEST);
@@ -152,24 +141,23 @@ void drawCube()
 void draw3DAXis()
 {
     glPushMatrix();
-    glLoadIdentity();
     glBegin(GL_LINES);
     // X
     glColor4f(YELLOW);
-    glVertex3f(-1.0f, 0.0f, 0.0f);
-    glVertex3f(1.0f, 0.0f, 0.0f);
+    glVertex3f(-1.0f * ESCALADO_FIG, 0.0f, 0.0f);
+    glVertex3f(1.0f * ESCALADO_FIG, 0.0f, 0.0f);
     // Y
     glColor4f(GREEN);
-    glVertex3f(0.0f, -1.0f, 0.0f);
-    glVertex3f(0.0f, 1.0f, 0.0f);
+    glVertex3f(0.0f, -1.0f * ESCALADO_FIG, 0.0f);
+    glVertex3f(0.0f, 1.0f * ESCALADO_FIG, 0.0f);
     glEnd();
 
     // Z
-    glRotatef(180.0f, 1.0f, 1.0f, 1.0f);
+    // glRotatef(180.0f, 1.0f, 1.0f, 1.0f);
     glBegin(GL_LINES);
     glColor4f(BLUE);
-    glVertex3f(0.0f, 0.0f, -1.0f);
-    glVertex3f(0.0f, 0.0f, 1.0f);
+    glVertex3f(0.0f, 0.0f, -1.0f * ESCALADO_FIG);
+    glVertex3f(0.0f, 0.0f, 1.0f * ESCALADO_FIG);
     glEnd();
     glPopMatrix();
 }
@@ -177,32 +165,31 @@ void draw3DAXis()
 void drawPlanes()
 {
     glPushMatrix();
-    glLoadIdentity();
     glBegin(GL_QUADS);
     // X - Y
-    glColor4f(YELLOW);
-    glVertex3f(-1.0f, 1.0f, 0.0f);  // arriba-izquierda
-    glVertex3f(-1.0f, -1.0f, 0.0f); // abajo-izquierda
-    glVertex3f(1.0f, -1.0f, 0.0f);  // abajo-derecha
-    glVertex3f(1.0f, 1.0f, 0.0f);   // arriba-derecha
+    glColor4f(YELLOW_ALPHA);
+    glVertex3f(1.0f, 1.0f, 0.0f);
+    glVertex3f(-1.0f, 1.0f, 0.0f);
+    glVertex3f(-1.0f, -1.0f, 0.0f);
+    glVertex3f(1.0f, -1.0f, 0.0f);
     glEnd();
 
     // X - Z
     glBegin(GL_QUADS);
-    glColor4f(GREEN);
-    glVertex3f(-1.0f, 0.0f, -1.0f); // arriba-izquierda
-    glVertex3f(-1.0f, 0.0f, 1.0f);  // abajo-izquierda
-    glVertex3f(1.0f, 0.0f, -1.0f);  // abajo-derecha
-    glVertex3f(1.0f, 0.0f, 1.0f);   // arriba-derecha
+    glColor4f(GREEN_ALPHA);
+    glVertex3f(1.0f, 0.0f, 1.0f);
+    glVertex3f(-1.0f, 0.0f, 1.0f);
+    glVertex3f(-1.0f, 0.0f, -1.0f);
+    glVertex3f(1.0f, 0.0f, -1.0f);
     glEnd();
 
     // Y - Z
     glBegin(GL_QUADS);
-    glColor4f(BLUE);
-    glVertex3f(0.0f, -1.0f, -1.0f); // arriba-izquierda
-    glVertex3f(0.0f, -1.0f, 1.0f);  // abajo-izquierda
-    glVertex3f(0.0f, 1.0f, -1.0f);  // abajo-derecha
-    glVertex3f(0.0f, 1.0f, 1.0f);   // arriba-derecha
+    glColor4f(BLUE_ALPHA);
+    glVertex3f(0.0f, 1.0f, 1.0f);
+    glVertex3f(0.0f, -1.0f, 1.0f);
+    glVertex3f(0.0f, -1.0f, -1.0f);
+    glVertex3f(0.0f, 1.0f, -1.0f);
     glEnd();
     glPopMatrix();
 }
@@ -210,25 +197,28 @@ void drawPlanes()
 void display()
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glEnable(GL_LINE_SMOOTH | GL_DEPTH_TEST);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
 
+    gluLookAt(2.0, 2.0, 2.0,  // Eye position
+              0.0, 0.0, 0.0,  // Look-at position
+              0.0, 1.0, 0.0); // Up vector
+
     switch (fig)
     {
-    case 'c':
+    case '1':
         drawCube();
         break;
-    case 't':
+    case '2':
         drawTriangle();
         break;
-    case 'd':
+    case '3':
         drawDonut();
         break;
-    case 'k':
+    case '4':
         drawTetreaHedron();
         break;
-    case 'p':
+    case '5':
         drawTeaPot();
         break;
     default:
@@ -255,11 +245,11 @@ void manageKeyBoardInput(unsigned char key, int x, int y)
 
     switch (key)
     {
-    case 'c':
-    case 't':
-    case 'd':
-    case 'k':
-    case 'p':
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
         fig = key;
         break;
     case 'w':
@@ -268,7 +258,7 @@ void manageKeyBoardInput(unsigned char key, int x, int y)
     case 27: // 'ESC'
         displayAxis = !displayAxis;
         break;
-    case 'm':
+    case 'p':
         displayPlane = !displayPlane;
         break;
     default:
@@ -314,12 +304,11 @@ void reshape(GLsizei width, GLsizei height)
 
 void reshapeFrustum(GLsizei width, GLsizei height)
 {
-    glViewport(0, 0, (GLfloat)width, (GLfloat)height);
+    glViewport(0, 0, width, height);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    glFrustum(-1.0, 1.0, -1.0, 1.0, 1.0, 20.0);
+    glFrustum(-1.0, 1.0, -1.0, 1.0, 1.0, 10.0);
     glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
 }
 
 int main(int argc, char **argv)
@@ -330,21 +319,21 @@ int main(int argc, char **argv)
     // Indicamos como ha de ser la nueva ventana
     glutInitWindowPosition(100, 100);
     glutInitWindowSize(W_WIDTH, W_HEIGHT);
-    glutInitDisplayMode(GLUT_RGBA | GLUT_SINGLE | GLUT_DEPTH);
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
 
     // Creamos la nueva ventana
     glutCreateWindow("Etapa 3");
+    glEnable(GL_LINE_SMOOTH | GL_DEPTH_TEST);
 
     // Indicamos cuales son las funciones de redibujado e idle
     glutDisplayFunc(display);
-    glutReshapeFunc(reshape);
-    // glutReshapeFunc(reshapeFrustum);
+    // glutReshapeFunc(reshape);
+    glutReshapeFunc(reshapeFrustum);
     glutKeyboardFunc(manageKeyBoardInput);
     glutIdleFunc(idle);
 
     glClearColor(BG_COLOR);
     glClear(GL_COLOR_BUFFER_BIT);
-    glOrtho(-1.0, 1.0f, -1.0, 1.0f, -1.0, 1.0f);
 
     // Comienza la ejecución del core de GLUT
     glutMainLoop();
