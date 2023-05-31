75f59e15b6ea2b9c0c288f7d844242e28c775c6e
Sergimayol <sergi2808@gmail.com>
2023-05-31 12:21:22 +0200

    Cámara ya funciona :+1: :moyai:

---
+++ src/Etapa4/camera.c
@@ -0,0 +1,87 @@
+#include "camera.h"
+#include <math.h>
+#include <stdio.h>
+
+void initCamera(Camera *cam)
+{
+	cam->yaw = 0.0f;
+	cam->pitch = 0.0f;
+	cam->position = (Vector3f){0.0f, 0.0f, 5.0f};
+	cam->lookingAt = (Vector3f){
+		.x = cam->position.x + sin(cam->yaw),
+		.y = cam->position.y + sin(cam->pitch),
+		.z = cam->position.z - cos(cam->yaw)};
+	cam->up = (Vector3f){0.0f, 1.0f, 0.0f};
+}
+
+void calcTargetPoint(Camera* cam, float* x, float* y, float* z)
+{
+	*x = cam->position.x + sin(cam->yaw);
+	*y = cam->position.y + sin(cam->pitch);
+	*z = cam->position.z - cos(cam->yaw);
+}
+
+void rotateCameraRight(Camera *cam, float angle)
+{
+	cam->yaw += angle;
+}
+
+void rotateCameraLeft(Camera *cam, float angle)
+{
+	cam->yaw -= angle;
+}
+
+void rotateCameraUp(Camera *cam, float angle)
+{
+	cam->pitch -= angle;
+}
+
+void rotateCameraDown(Camera *cam, float angle)
+{
+	cam->pitch += angle;
+}
+
+void moveCameraUp(Camera *cam, float inc)
+{
+	cam->position.y += inc;
+}
+
+void moveCameraDown(Camera *cam, float inc)
+{
+	cam->position.y -= inc;
+}
+
+void moveCameraForward(Camera *cam, float inc)
+{
+	cam->position.x -= inc * sin(cam->yaw);
+	cam->position.z -= inc * cos(cam->yaw);
+}
+void moveCameraBackward(Camera *cam, float inc)
+{
+	cam->position.x += inc * sin(cam->yaw);
+	cam->position.z += inc * cos(cam->yaw);
+}
+
+void starfeCameraRight(Camera *cam, float inc)
+{
+	cam->position.x += inc * cos(cam->yaw);
+	cam->position.z -= inc * sin(cam->yaw);
+}
+
+void starfeCameraLeft(Camera *cam, float inc)
+{
+	cam->position.x -= inc * cos(cam->yaw);
+	cam->position.z += inc * sin(cam->yaw);
+}
+
+void printCamera(Camera *cam)
+{
+	printf("Camera:\n");
+	printf("\tYaw: %f\n", cam->yaw);
+	printf("\tPitch: %f\n", cam->pitch);
+	printf("\tPosition: (%f, %f, %f)\n", cam->position.x, cam->position.y, cam->position.z);
+	printf("\tLookingAt: (%f, %f, %f)\n", cam->lookingAt.x, cam->lookingAt.y, cam->lookingAt.z);
+	printf("\tUp: (%f, %f, %f)\n", cam->up.x, cam->up.y, cam->up.z);
+	printf("\n");
+}
+
