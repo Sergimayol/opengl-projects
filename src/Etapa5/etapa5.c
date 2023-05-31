bb47743739cef2a08ba392a0a95c2a1b42309ee3
Rubén Palmer <40397192+Zygmut@users.noreply.github.com>
2023-05-31 15:46:41 +0200

    base light system

--- src/Etapa5/etapa5.c
+++ src/Etapa5/etapa5.c
@@ -21,19 +21,61 @@ float lookAtZ = 0.0f;
 float upX = 0.0f;
 float upY = 1.0f;
 float upZ = 0.0f;
-// Set light properties
-GLfloat light_position[] = {0.0f, 2.0f, 0.0f, 0.0f}; // Light position
-GLfloat light_position1[] = {2.0f, 0.0f, 0.0f, 0.0f};
-GLfloat light_position2[] = {0.0f, 0.0f, 2.0f, 0.0f};
-// Set material properties
-float fAngulo = 0.0f;
 
+// Set light properties
+GLfloat light_positions[3][4] = {
+	{0.0f, 2.0f, 0.0f, 0.0f},
+	{2.0f, 0.0f, 0.0f, 0.0f},
+	{-2.0f, 0.0f, 0.0f, 0.0f}};
+struct Color3f light_colors[3] = {
+	{1.0f, 1.0f, 1.0f},
+	{0.89803921569f, 0.78431372549f, 0.56470588235f},
+	{0.65098039216f, 0.81960784314f, 0.53725490196f}};
+bool light_status[] = {true, false, false, false};
+
+float fAngulo = 0.0f;
 float rotationCoords[] = {0.0f, 1.0f, 0.0f};
 struct Color3f figColor = {0.251f, 0.51f, 0.427f};
 
 bool isFlat = false;
-bool light1On = false;
-bool light2On = false;
+
+void draw_figure(unsigned char index)
+{
+
+	switch (index)
+	{
+	case '1':
+		drawTeapot(0.5 * ESCALADO_FIG, fAngulo, rotationCoords, displayWired, figColor);
+		break;
+	case '2':
+		drawDonut(0.5 * ESCALADO_FIG, 20, fAngulo, rotationCoords, displayWired, figColor);
+		break;
+	default:
+		break;
+	}
+}
+void manage_lights()
+{
+	const int arr_len = sizeof(light_status) / sizeof(light_status[0]);
+	for (int i = 0; i < arr_len; i++)
+	{
+		if (light_status[i])
+		{
+			glEnable(GL_LIGHT0 + i);
+
+			glDisable(GL_LIGHTING);
+			glPushMatrix();
+			glTranslatef(light_positions[i][0], light_positions[i][1], light_positions[i][2]);
+			drawSphere(0.1 * ESCALADO_FIG, 20, 20, rotationCoords, false, light_colors[i]);
+			glPopMatrix();
+			glEnable(GL_LIGHTING);
+		}
+		else
+		{
+			glDisable(GL_LIGHT0 + i);
+		}
+	}
+}
 
 void display()
 {
@@ -46,75 +88,13 @@ void display()
 			  lookAtX, lookAtY, lookAtZ,				   // Look-at position
 			  upX, upY, upZ);							   // Up vector
 
-	switch (fig)
-	{
-	case '1':
-		drawTeapot(0.5 * ESCALADO_FIG, fAngulo, rotationCoords, displayWired, figColor);
-		break;
-	case '2':
-		drawDonut(0.5 * ESCALADO_FIG, 20, fAngulo, rotationCoords, displayWired, figColor);
-		break;
-	default:
-		break;
-	}
-
-	if (isFlat)
-	{
-		glShadeModel(GL_FLAT);
-	}
-	else
-	{
-		glShadeModel(GL_SMOOTH);
-	}
-
-	if (light1On)
-	{
-		glEnable(GL_LIGHT1);
-	}
-	else
-	{
-		glDisable(GL_LIGHT1);
-	}
-
-	if (light2On)
-	{
-		glEnable(GL_LIGHT2);
-	}
-	else
-	{
-		glDisable(GL_LIGHT2);
-	}
+	manage_lights();
+	draw_figure(fig);
+	glShadeModel(isFlat ? GL_FLAT : GL_SMOOTH);
 
 	glDisable(GL_LIGHTING);
-	if (light1On)
-	{
-		glPushMatrix();
-		glTranslatef(light_position1[0], light_position1[1], light_position1[2]);
-		drawSphere(0.1 * ESCALADO_FIG, 20, 20, rotationCoords, false, (struct Color3f){0.89803921569f, 0.78431372549f, 0.56470588235f});
-		glPopMatrix();
-	}
-	if (light2On)
-	{
-		glPushMatrix();
-		glTranslatef(light_position2[0], light_position2[1], light_position2[2]);
-		drawSphere(0.1 * ESCALADO_FIG, 20, 20, rotationCoords, false, (struct Color3f){0.65098039216f, 0.81960784314f, 0.53725490196f});
-		glPopMatrix();
-	}
-	if (displayPlane)
-	{
-		drawPlanes(ESCALADO_FIG);
-	}
-
-	if (displayAxis)
-	{
-		draw3DAXis(ESCALADO_FIG + 0.2f);
-	}
-
-	glPushMatrix();
-	glTranslatef(light_position[0], light_position[1], light_position[2]);
-	drawSphere(0.1 * ESCALADO_FIG, 20, 20, rotationCoords, false, (struct Color3f){1.0f, 1.0f, 1.0f});
-	glPopMatrix();
-
+	displayPlane ? drawPlanes(ESCALADO_FIG) : NULL;
+	displayAxis ? draw3DAXis(ESCALADO_FIG + 0.2f) : NULL;
 	glEnable(GL_LIGHTING);
 
 	glutSwapBuffers();
@@ -123,34 +103,27 @@ void display()
 void init_lights()
 {
 	const GLfloat mat_ambient[] = {0.1f, 0.1f, 0.1f, 1.0f};
-	const GLfloat mat_diffuse[] = {0.8f, 0.8f, 0.8f, 1.0f};
-	const GLfloat mat_specular[] = {1.0f, 1.0f, 1.0f, 1.0f};
-	const GLfloat mat_shininess[] = {50.0f};
-
-	glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, mat_ambient);
-	glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, mat_diffuse);
-	glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, mat_specular);
-	glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, mat_shininess);
-	// Enable lighting
-	glEnable(GL_LIGHTING);
-	glEnable(GL_LIGHT0);
-
+	const GLfloat mat_color[] = {LIGHT}; // Ambient light color
+	const GLfloat mat_shininess[] = {15.0f};
 	const GLfloat light_ambient[] = {BG_COLOR}; // Ambient light color
 
-	glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
-	glLightfv(GL_LIGHT0, GL_DIFFUSE, (GLfloat[]){LIGHT});
-	glLightfv(GL_LIGHT0, GL_SPECULAR, (GLfloat[]){PURE_WHITE});
-	glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+	glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, mat_ambient);
+	glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, mat_color);
+	glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, mat_color);
+	glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, mat_shininess);
 
-	glLightfv(GL_LIGHT1, GL_AMBIENT, light_ambient);
-	glLightfv(GL_LIGHT1, GL_DIFFUSE, (GLfloat[]){YELLOW});
-	glLightfv(GL_LIGHT1, GL_SPECULAR, (GLfloat[]){YELLOW});
-	glLightfv(GL_LIGHT1, GL_POSITION, light_position1);
+	// Enable lighting
+	glEnable(GL_LIGHTING);
 
-	glLightfv(GL_LIGHT2, GL_AMBIENT, light_ambient);
-	glLightfv(GL_LIGHT2, GL_DIFFUSE, (GLfloat[]){BLUE});
-	glLightfv(GL_LIGHT2, GL_SPECULAR, (GLfloat[]){BLUE});
-	glLightfv(GL_LIGHT2, GL_POSITION, light_position2);
+	const int arr_len = sizeof(light_status) / sizeof(light_status[0]);
+	for (int i = 0; i < arr_len; i++)
+	{
+		glLightfv(GL_LIGHT0 + i, GL_AMBIENT, light_ambient);
+		glLightfv(GL_LIGHT0 + i, GL_DIFFUSE, (GLfloat[]){light_colors[i].r, light_colors[i].g, light_colors[i].b});
+		glLightfv(GL_LIGHT0 + i, GL_SPECULAR, (GLfloat[]){light_colors[i].r, light_colors[i].g, light_colors[i].b});
+		glLightfv(GL_LIGHT0 + i, GL_POSITION, light_positions[i]);
+	}
+	glEnable(GL_LIGHT0);
 }
 
 void idle()
@@ -192,33 +165,33 @@ void manageKeyBoardInput(unsigned char key, int x, int y)
 		break;
 	case 'a':
 		// pos x
-		light_position[0] += inc;
-		glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+		light_positions[0][0] += inc;
+		glLightfv(GL_LIGHT0, GL_POSITION, light_positions[0]);
 		break;
 	case 'z':
 		// pos x
-		light_position[0] -= inc;
-		glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+		light_positions[0][0] -= inc;
+		glLightfv(GL_LIGHT0, GL_POSITION, light_positions[0]);
 		break;
 	case 's':
 		// pos y
-		light_position[1] += inc;
-		glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+		light_positions[0][1] += inc;
+		glLightfv(GL_LIGHT0, GL_POSITION, light_positions[0]);
 		break;
 	case 'x':
 		// pos y
-		light_position[1] -= inc;
-		glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+		light_positions[0][1] -= inc;
+		glLightfv(GL_LIGHT0, GL_POSITION, light_positions[0]);
 		break;
 	case 'd':
 		// pos z
-		light_position[2] += inc;
-		glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+		light_positions[0][2] += inc;
+		glLightfv(GL_LIGHT0, GL_POSITION, light_positions[0]);
 		break;
 	case 'c':
 		// pos z
-		light_position[2] -= inc;
-		glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+		light_positions[0][2] -= inc;
+		glLightfv(GL_LIGHT0, GL_POSITION, light_positions[0]);
 		break;
 	case 32: // Space
 		// Change light mode
@@ -240,10 +213,16 @@ void extraKeyBoardInput(int key, int x, int y)
 	switch (key)
 	{
 	case GLUT_KEY_F1:
-		light1On = !light1On;
+		light_status[0] = !light_status[0];
 		break;
 	case GLUT_KEY_F2:
-		light2On = !light2On;
+		light_status[1] = !light_status[1];
+		break;
+	case GLUT_KEY_F3:
+		light_status[2] = !light_status[2];
+		break;
+	case GLUT_KEY_F4:
+		light_status[3] = !light_status[3];
 		break;
 	default:
 		break;
