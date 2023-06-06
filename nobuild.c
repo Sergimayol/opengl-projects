6ba2ba80b993e641c424f479aa4b4c1fbac5d1cb
Sergimayol <sergi2808@gmail.com>
2023-06-06 13:10:30 +0200

    Update nobuild.c

--- nobuild.c
+++ nobuild.c
@@ -17,7 +17,7 @@ int main(int argc, char **argv)
 	//CMD(COMPILER, "-o", BUILD_PATH("etapa3"), "./src/Etapa3/etapa3.c", CFLAGS);
 	//CMD(COMPILER, "-o", BUILD_PATH("etapa4"), "./src/Etapa4/etapa4.c", "./src/Etapa4/AxisAndPlanes.c", "./src/Etapa4/figures.c", "./src/Etapa4/camera.c", CFLAGS);
 	//CMD(COMPILER, "-o", BUILD_PATH("etapa5"), "./src/Etapa5/etapa5.c", "./src/Etapa5/AxisAndPlanes.c", "./src/Etapa5/figures.c", "./src/Etapa5/light.c", CFLAGS);
-	CMD(COMPILER, "-o", BUILD_PATH("etapa6"), "./src/Etapa6/etapa6.c", "./src/Etapa6/AxisAndPlanes.c", "./src/Etapa6/figures.c", "./src/Etapa6/light.c", "./src/Etapa6/camera.c", "./src/Etapa6/object.c", "./src/Etapa6/texture.c", "./src/Etapa6/map.c", CFLAGS, EXTRA_FLAGS);
+	CMD(COMPILER, "-O3", "-o", BUILD_PATH("etapa6"), "./src/Etapa6/etapa6.c", "./src/Etapa6/AxisAndPlanes.c", "./src/Etapa6/figures.c", "./src/Etapa6/light.c", "./src/Etapa6/camera.c", "./src/Etapa6/object.c", "./src/Etapa6/texture.c", "./src/Etapa6/map.c", CFLAGS, EXTRA_FLAGS);
 	//CMD(COMPILER, "-o", BUILD_PATH("base"), "./src/templates/base.c", CFLAGS);
 	return 0;
 }
