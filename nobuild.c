2be63f8ac5281fe19a112357e0c614f70af7b709
Sergimayol <sergi2808@gmail.com>
2023-06-12 11:06:48 +0200

    Habilitar compilación de todas las etapas

--- nobuild.c
+++ nobuild.c
@@ -11,13 +11,13 @@ int main(int argc, char **argv)
 {
 	GO_REBUILD_URSELF(argc, argv);
 	MKDIRS(BUILD_DIR);
-	//CMD(COMPILER, "-o", BUILD_PATH("etapa1"), "./src/Etapa1/etapa1.c", CFLAGS);
-	//CMD(COMPILER, "-o", BUILD_PATH("etapa2"), "./src/Etapa2/etapa2.c", CFLAGS);
-	//CMD(COMPILER, "-o", BUILD_PATH("pendulo"), "./src/Etapa2/pendulo.c", CFLAGS);
-	//CMD(COMPILER, "-o", BUILD_PATH("etapa3"), "./src/Etapa3/etapa3.c", CFLAGS);
-	//CMD(COMPILER, "-o", BUILD_PATH("etapa4"), "./src/Etapa4/etapa4.c", "./src/Etapa4/AxisAndPlanes.c", "./src/Etapa4/figures.c", "./src/Etapa4/camera.c", CFLAGS);
-	//CMD(COMPILER, "-o", BUILD_PATH("etapa5"), "./src/Etapa5/etapa5.c", "./src/Etapa5/AxisAndPlanes.c", "./src/Etapa5/figures.c", "./src/Etapa5/light.c", CFLAGS);
+	CMD(COMPILER, "-o", BUILD_PATH("etapa1"), "./src/Etapa1/etapa1.c", CFLAGS);
+	CMD(COMPILER, "-o", BUILD_PATH("etapa2"), "./src/Etapa2/etapa2.c", CFLAGS);
+	CMD(COMPILER, "-o", BUILD_PATH("pendulo"), "./src/Etapa2/pendulo.c", CFLAGS);
+	CMD(COMPILER, "-o", BUILD_PATH("etapa3"), "./src/Etapa3/etapa3.c", CFLAGS);
+	CMD(COMPILER, "-o", BUILD_PATH("etapa4"), "./src/Etapa4/etapa4.c", "./src/Etapa4/AxisAndPlanes.c", "./src/Etapa4/figures.c", "./src/Etapa4/camera.c", CFLAGS);
+	CMD(COMPILER, "-o", BUILD_PATH("etapa5"), "./src/Etapa5/etapa5.c", "./src/Etapa5/AxisAndPlanes.c", "./src/Etapa5/figures.c", "./src/Etapa5/light.c", CFLAGS);
 	CMD(COMPILER, "-O3", "-o", BUILD_PATH("etapa6"), "./src/Etapa6/etapa6.c", "./src/Etapa6/AxisAndPlanes.c", "./src/Etapa6/figures.c", "./src/Etapa6/light.c", "./src/Etapa6/camera.c", "./src/Etapa6/object.c", "./src/Etapa6/texture.c", "./src/Etapa6/map.c", CFLAGS, EXTRA_FLAGS);
-	//CMD(COMPILER, "-o", BUILD_PATH("base"), "./src/templates/base.c", CFLAGS);
+	CMD(COMPILER, "-o", BUILD_PATH("base"), "./src/templates/base.c", CFLAGS);
 	return 0;
 }
--- src/utils/AxisAndPlanes.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-// Dibuja los ejes X Y Z
-void draw3DAXis(const float ESCALADO);
-// Dibuja los planos de los ejes
-void drawPlanes(const float ESCALADO);
